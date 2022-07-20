77fe1c215ee599e037615b11aa8bff1051159cc9
tssandeep <109350985+tssandeep@users.noreply.github.com>
2022-07-20 13:59:16 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,1 +1,8 @@
-public class aiml1 { public static void main(String[] args) { System.out.println("AIML, PROJECT"); } }
+/** 
+ * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
+ */
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+  }
+}
